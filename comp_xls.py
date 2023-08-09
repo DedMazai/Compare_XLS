6942c2a396a5dc098ecad01129d099ebbee96797
DedMazai <byurav@rambler.ru>
2023-08-10 00:07:41 +0300

    закачуємо в мейн з розбіжністю в кількості 10%

--- comp_xls.py
+++ comp_xls.py
@@ -8,12 +8,17 @@ data_client.rename(columns={'К-сть': "Кількість"}, inplace=True)
 data_importer.dropna(subset=['Товар', 'Кількість'], inplace=True)
 data_client.dropna(subset=['Товар', 'Кількість'], inplace=True)
 
-data_importer['Кількість'] = data_importer['Кількість'].apply(lambda x: str(x).split()[0].replace('.', ','))
-data_client['Кількість'] = data_client['Кількість'].apply(lambda x: x.split(' ')[0])
+data_importer['Кількість'] = data_importer['Кількість'].apply(lambda x: float(x))
+data_client['Кількість'] = data_client['Кількість'].apply(lambda x: float(x.split(' ')[0].replace(',', '.')))
+
 
 data_with_values = data_importer.merge(data_client, how='outer', on='Товар',
                                        suffixes=('_постач', '_замов'))
 
 data_with_values = data_with_values[data_with_values['Кількість_постач'] != data_with_values['Кількість_замов']]
+data_with_values = data_with_values.fillna(0)
+data_with_values['Різниця_кількість'] = abs(data_with_values['Кількість_постач'] - data_with_values['Кількість_замов'])
+
+data_with_values = data_with_values.loc[data_with_values['Різниця_кількість'] > data_with_values['Кількість_замов'] * 0.2]
 
 data_with_values.to_excel('difference.xlsx')
