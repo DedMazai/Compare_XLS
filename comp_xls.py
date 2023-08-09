ad538dfa9daa34809d5131a1e79529111d40df0f
DedMazai <byurav@rambler.ru>
2023-08-10 00:24:32 +0300

    розбіжністю в кількості 10%

--- comp_xls.py
+++ comp_xls.py
@@ -16,6 +16,7 @@ data_with_values = data_importer.merge(data_client, how='outer', on='Товар'
 
 data_with_values = data_with_values[data_with_values['Кількість_постач'] != data_with_values['Кількість_замов']]
 data_with_values = data_with_values.fillna(0)
+
 data_with_values['Різниця_кількість'] = abs(data_with_values['Кількість_постач'] - data_with_values['Кількість_замов'])
 
 data_with_values = data_with_values.loc[data_with_values['Різниця_кількість'] > data_with_values['Кількість_замов'] * 0.2]
