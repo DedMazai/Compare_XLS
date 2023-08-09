bfa2485d5b2ece79938d8d04afd6a417601bcfe6
DedMazai <byurav@rambler.ru>
2023-08-09 23:07:24 +0300

    закачуємо в мейн

--- comp_xls.py
+++ comp_xls.py
@@ -1,2 +1,19 @@
+import pandas as pd
 
+data_importer = pd.read_excel('699.xls', header=11, usecols=['Товар', 'Кількість'])
+data_client = pd.read_excel('export_products_230725.xlsx', header=1, usecols=['Товар', 'К-сть'], sheet_name=0)
 
+data_client.rename(columns={'К-сть': "Кількість"}, inplace=True)
+
+data_importer.dropna(subset=['Товар', 'Кількість'], inplace=True)
+data_client.dropna(subset=['Товар', 'Кількість'], inplace=True)
+
+data_importer['Кількість'] = data_importer['Кількість'].apply(lambda x: str(x).split()[0].replace('.', ','))
+data_client['Кількість'] = data_client['Кількість'].apply(lambda x: x.split(' ')[0])
+
+data_with_values = data_importer.merge(data_client, how='outer', on='Товар',
+                                       suffixes=('_постач', '_замов'))
+
+data_with_values = data_with_values[data_with_values['Кількість_постач'] != data_with_values['Кількість_замов']]
+
+data_with_values.to_excel('difference.xlsx')
